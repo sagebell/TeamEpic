7413dd8c1ebc1380ab268b64fb0a3f84d9ef199c
Justin Tyrrell <jet1138@stu.aii.edu>
2014-08-11 19:45:42 -0700

    Basic Scripts and beginning of Game Logic

---
+++ AsylumProject/Assets/Scripts/ButtonLightBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonLightBehaviour : MonoBehaviour {
+	public GameObject lightObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUpAsButton() {
+		lightObject.SetActive(!lightObject.activeSelf);
+	}
+}
