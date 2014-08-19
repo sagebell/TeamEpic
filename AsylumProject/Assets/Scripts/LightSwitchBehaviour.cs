26f01f8a12aef54b41869300ebaaedce330befee
Justin Tyrrell <jet1138@stu.aii.edu>
2014-08-19 00:17:46 -0700

    Uploading basic scripts for update during the group GIT project in class Week 7

---
+++ AsylumProject/Assets/Scripts/LightSwitchBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitchBehaviour : MonoBehaviour {
+	public GameObject lightObject;
+
+	//private 
+
+	void OnMouseUpAsButton() {
+		lightObject.SetActive(!lightObject.activeSelf);
+	}
+}
