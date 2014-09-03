109cbe44c606825e8b9d209c5a85a4726d61453c
Jason Dean Crossley <Rojibora@gmail.com>
2014-09-02 19:57:26 -0700

    Bullshit Commit

--- AsylumProject/Assets/Scripts/jasonTestUpload.cs
+++ AsylumProject/Assets/Scripts/jasonTestUpload.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class JasonUpdate : MonoBehavior {
+
+    private bool _bullSHit = true;
 	void Start () 
 	{
-	
+	    // This shit is seriously fucking lame. serisouly... Guck Fit
+
 	}
 	
 	void Update () 
