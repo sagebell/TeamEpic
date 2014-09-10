372f0c66d4730b451f1a9191579057cf4456b24d
Michael DeLomba <michael.delomba@stu.aii.edu>
2014-09-09 19:20:44 -0700

    Reduce/Induce/Horror Scripts and Horror Meter GUI

--- AsylumProject/Assets/Scripts/PlayerScript.cs
+++ AsylumProject/Assets/Scripts/PlayerScript.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
 	public GameObject leftHandTarget = null;
 	public GameObject rightHandTarget = null;
+	public float HorrorMeterMax = 100.0f;
+
+	public float HorrorMeter = 0.0f;
 
 	private DATACORE dataCore = null;
+	    public float hSbarValue;
 
 
 	// Use this for initialization
 	void Start () {
+		
 		if (dataCore == null) {
 			dataCore = GameObject.FindGameObjectWithTag("GameLogic").GetComponent<DATACORE>();
 		}
@@ -25,6 +30,11 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		HorrorMeter -= 0.001f;
+
+		if (HorrorMeter <= 0f) HorrorMeter = 0f;
+		if (HorrorMeter >= 100.0f) HorrorMeter = 100.0f;
+
+		
 	}
 }
